9d1f88a15d5604a9b076e3d0bcf87299e8f1519b
Gabau <au.gabriel@yahoo.com.sg>
2022-04-07 23:46:40 +0800

    Modify frontend tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders logo', () => {
+  const container = document.getElementById('root');
+  if (!container) return;
+  createRoot(container).render(<App />);
+  const img = screen.getByRole('img');
+  expect(img).toBeInTheDocument();
 });
